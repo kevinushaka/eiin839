c7258d272cc9b439000a1e49144eb1bc81e05aa8
kevinushaka <keushakaku@outlook.com>
2021-03-16 14:07:46 +0100

    First Web Client SOAP

---
+++ WebServiceClientSOAP/WebServiceClientSOAP/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using WebServiceClientSOAP.Calculator;
+namespace WebServiceClientSOAP
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+            
+        {
+            CalculatorSoapClient client= new CalculatorSoapClient();
+            Console.WriteLine(client.Add(1, 2));
+            Console.ReadLine();
+        }
+    }
+}
